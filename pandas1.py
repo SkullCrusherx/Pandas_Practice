d8fb374f13023ad40408dac1b87081a81a7e6580
Predator <kungfuyoga99@gmail.com>
2026-03-30 21:07:13 +0530

    auto commit

--- pandas1.py
+++ pandas1.py
@@ -2,15 +2,16 @@
 import pandas as pd
 
 list = [1,2,3,4,5,6,7,8,9,10]
+
 dic = {
     "name":["Sayan","Rupa","Rahul"],
     "age":[26,21,16],
     "city":["Delhi","Goa","Punjab"]
     }
 
-
 Siries = pd.Series(list)
-#print(Siries)
+print(Siries)
+
 Dic1 = pd.Series(dic)
 print(Dic1)
 
@@ -18,6 +19,3 @@ print("=========================")
 
 Dic2 = pd.DataFrame(dic)
 print(Dic2)
-
-
-
