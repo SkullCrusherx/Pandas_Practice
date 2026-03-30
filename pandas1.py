1ec26bd141c38508c146fa9beb446a8adf0a8e76
Predator <kungfuyoga99@gmail.com>
2026-03-30 22:20:16 +0530

    auto commit

--- pandas1.py
+++ pandas1.py
@@ -20,6 +20,6 @@ print(Siries_normal) #Siries make 1D From List
 print(Siries_index) #index change and update
 print(Siries_type) #show data type
 print(Siries_ch_type) #chnage the data type
-print("FF \n",Siries_only_int)
+print("FF\n",Siries_only_int)
 Dic1 = pd.Series(dic)
 print(Dic1) #Siries make 2D From Dictionary
