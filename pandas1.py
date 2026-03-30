81905ba987582cdac16bc872d3782f32fd55f73c
Predator <kungfuyoga99@gmail.com>
2026-03-30 22:21:16 +0530

    auto commit

--- pandas1.py
+++ pandas1.py
@@ -9,17 +9,18 @@ dic = {
     "city":["Delhi","Goa","Punjab"]
     }
 
-
+#-------------------------------------------------------------------------
 Siries_normal = pd.Series(list) #showing Data
 Siries_index = pd.Series(list,index=["Sayan","Rupa","Rahul","Punjab"])
 Siries_type = pd.Series(type(list))
 Siries_ch_type = pd.Series(list,dtype=object)
 Siries_only_int = pd.Series(data,index=["Sayan","Rupa","Rahul","Punjab"])
-
+#-------------------------------------------------------------------------
 print(Siries_normal) #Siries make 1D From List
 print(Siries_index) #index change and update
 print(Siries_type) #show data type
 print(Siries_ch_type) #chnage the data type
-print(Siries_only_int)
+print(Siries_only_int) #single number make series with index how much need also
+#-------------------------------------------------------------------------
 Dic1 = pd.Series(dic)
 print(Dic1) #Siries make 2D From Dictionary
