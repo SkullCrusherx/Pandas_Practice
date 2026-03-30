69bb7a253fd83f7349a36f669acd60c6bf98aae8
Predator <kungfuyoga99@gmail.com>
2026-03-30 22:18:16 +0530

    auto commit

--- pandas1.py
+++ pandas1.py
@@ -8,10 +8,12 @@ dic = {
     "age":[26,21,16],
     "city":["Delhi","Goa","Punjab"]
     }
+
 Siries_normal = pd.Series(list) #showing Data
 Siries_index = pd.Series(list,index=["Sayan","Rupa","Rahul","Punjab"])
 Siries_type = pd.Series(type(list))
 Siries_ch_type = pd.Series(list,dtype=object)
+
 print(Siries_normal) #Siries make 1D From List
 print(Siries_index) #index change and update
 print(Siries_type) #show data type
