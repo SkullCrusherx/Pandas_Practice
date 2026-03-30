6f5a3b8d218d0b0a138297bf7ae6fb2340f7a4fb
Predator <kungfuyoga99@gmail.com>
2026-03-30 23:01:50 +0530

    auto commit

--- pandas1.py
+++ pandas1.py
@@ -27,8 +27,8 @@ print(Siries_sepcs_data)
 print(Siries_normal_name)
 #-------------------------------------------------------------------------
 Dic1 = pd.Series(dic)
-
 print(Dic1) #Siries make 2D From Dictionary
 print(Dic1["name"][1])
 #for specific Dictionary data ["name"] for Dictionary [] next one index value to show element for the data
 print(Dic1["age"]) #For specific Dictionary age
+#-------------------------------------------------------------------------
