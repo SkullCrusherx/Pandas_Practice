e80f9791426698a9b92354d4916350c242c4b7f4
Predator <kungfuyoga99@gmail.com>
2026-03-30 22:19:51 +0530

    auto commit

--- pandas1.py
+++ pandas1.py
@@ -2,22 +2,24 @@
 import pandas as pd
 
 list = [1,2,3,4]
-
+data = 10
 dic = {
     "name":["Sayan","Rupa","Rahul"],
     "age":[26,21,16],
     "city":["Delhi","Goa","Punjab"]
     }
 
+
 Siries_normal = pd.Series(list) #showing Data
 Siries_index = pd.Series(list,index=["Sayan","Rupa","Rahul","Punjab"])
 Siries_type = pd.Series(type(list))
 Siries_ch_type = pd.Series(list,dtype=object)
+Siries_only_int = pd.Series(data,index=["Sayan","Rupa","Rahul","Punjab"])
 
 print(Siries_normal) #Siries make 1D From List
 print(Siries_index) #index change and update
 print(Siries_type) #show data type
 print(Siries_ch_type) #chnage the data type
-
+print("FF",Siries_only_int)
 Dic1 = pd.Series(dic)
 print(Dic1) #Siries make 2D From Dictionary
