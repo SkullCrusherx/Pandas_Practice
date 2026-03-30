1da260e4f1c669e680b43ee79c052c2f4db1077f
Predator <kungfuyoga99@gmail.com>
2026-03-30 22:57:49 +0530

    auto commit

--- pandas1.py
+++ pandas1.py
@@ -29,5 +29,6 @@ print(Siries_normal_name)
 Dic1 = pd.Series(dic)
 
 print(Dic1) #Siries make 2D From Dictionary
-print(Dic1["name"][1]) #for specific Dictionary data
+print(Dic1["name"][1])
+#for specific Dictionary data ["name"] for Dictionary [] next one index value to show element for the data
 print(Dic1["age"]) #for specific Dictionary age
