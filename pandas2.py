f5b99b1e17cedfb8d982eca764db82193f16c3d0
Predator <kungfuyoga99@gmail.com>
2026-03-31 20:36:19 +0530

    auto commit

--- pandas2.py
+++ pandas2.py
@@ -1,2 +1,16 @@
 import pandas as pd
 
+dic = {
+    "A":[1,2,3,4,5,6],
+    "B":[7,8,9,10,11,12]
+}
+list = [1,2,3,4,5,6]
+list_2d = [[1,2,3,4],[5,6,7,8],[9,10,11,12]]
+dframe = pd.DataFrame(dic)
+frame = pd.DataFrame(list)
+Gmm = pd.DataFrame(list_2d)
+print(dframe)
+print("=============" * 3)
+print(frame)
+print("=============" * 3)
+print(Gmm)
