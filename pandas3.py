2763baf3bee8cce428c550f7d27a7086184c331b
Predator <kungfuyoga99@gmail.com>
2026-04-05 01:52:48 +0530

    auto commit

--- pandas3.py
+++ pandas3.py
@@ -2,12 +2,12 @@ import pandas as pd
 
 csv = pd.read_csv("ML.csv")
 
-print(csv.index)        #Showing the Detail of rangeindex like step start end all
-print(csv.columns)      #showing all the column name mean header also
-print(csv.describe())   # showing here count mean max and other values also
-print(csv.head(3))      #3 here how much data need to show here from Top
-print(csv.tail(3))      #3 here how much data need to show here from Bottom
-print(csv[:2])          #from start to end also we check print using like these [start:end]
-print(type(csv[:2]))    #showing or reflect the data type
-print(csv.index.array)
-print(csv.columns.array)
+print(csv.index)            #Showing the Detail of rangeindex like step start end all
+print(csv.columns)          #showing all the column name mean header also
+print(csv.describe())       #Showing here count mean max and other values also
+print(csv.head(3))          #3 here how much data need to show here from Top
+print(csv.tail(3))          #3 here how much data need to show here from Bottom
+print(csv[:2])              #from start to end also we check print using like these [start:end]
+print(type(csv[:2]))        #showing or reflect the data type
+print(csv.index.array)      #showing index all number into array(panda)
+print(csv.columns.array)    #showing in array Column name or headers
