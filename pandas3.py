f3843bafbb7fb0abefc2652c6455e70cabc4735e
Predator <kungfuyoga99@gmail.com>
2026-04-05 01:49:25 +0530

    auto commit

--- pandas3.py
+++ pandas3.py
@@ -8,3 +8,4 @@ print(csv.describe())   # showing here count mean max and other values also
 print(csv.head(3))      #3 here how much data need to show here from Top
 print(csv.tail(3))      #3 here how much data need to show here from Bottom
 print(csv[:2])          #from start to end also we check print using like these [start:end]
+print(type(csv[:2]))
