f17f294b465d6b2ba203e1ad42dbe0416b9bb044
Predator <kungfuyoga99@gmail.com>
2026-04-05 01:47:13 +0530

    auto commit

--- pandas3.py
+++ pandas3.py
@@ -1,5 +1,10 @@
 import pandas as pd
 
+csv = pd.read_csv("ML.csv")
 
-csv = pd.read_csv("ML.csv",header=None,names=["A","A2"])
-print(csv)
+print(csv.index)        #Showing the Detail of rangeindex like step start end all
+print(csv.columns)      #showing all the column name mean header also
+print(csv.describe())   # showing here count mean max and other values also
+print(csv.head(3))      #3 here how much data need to show here from Top
+print(csv.tail(3))      #3 here how much data need to show here from Bottom
+print(csv[:2])          #from start to end also we check print using like these [start:end]
