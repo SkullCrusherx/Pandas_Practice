7ddadd5d5f37321ac3dc211c465145366784057a
Predator <kungfuyoga99@gmail.com>
2026-04-06 00:28:30 +0530

    auto commit

--- pandas4.py
+++ pandas4.py
@@ -1,4 +1,19 @@
 #DataFrame
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
 
-csv = pd.read_csv("./data/train.csv")
+csv = pd.read_csv("new.csv",usecols=["Day"],skiprows=0)
+csv1 = pd.read_csv("new.csv",usecols=["Time"],skiprows=0)
+Days = np.array(csv).reshape(-1)
+Work_Time = np.array(csv1).reshape(-1)
+
+plt.title("Office Work Entry")
+plt.xlabel("Day")
+plt.ylabel("Time")
+plt.bar(Days,Work_Time)
+plt.xticks(Days)
+plt.show()
+
+
+
