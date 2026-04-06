a49a437118d3305967b320af3b098129c5204ab6
Predator <kungfuyoga99@gmail.com>
2026-04-06 21:15:53 +0530

    auto commit

--- pandas2.py
+++ pandas2.py
@@ -13,6 +13,7 @@ var_1 = pd.DataFrame(list)                                 # Make List 2D DataFr
 var_2 = pd.DataFrame(list_1)                               # Make list 2D Dataframe but Row is 2 and Column element of Dataset
 var_3 = pd.DataFrame(Dic)                                  # Here 2D key make Column Name and values [list] which is Given is the element (equal Data length mandatory or value error reflect)
 var_4 = pd.DataFrame(Dic,columns=["A"])                    # Given columns to check specific column elements
+var_5 = pd.DataFrame(Dic,index=["C","D"])
 
 
 print(var_1)                                                # Reflect list Data
@@ -22,3 +23,4 @@ print(type(var_1))                                          # Print which functi
 print(var_2[0][0])                                          # according to List var print only specific from list to list Data then 1st whch list then row like var_2[list_number][row_number]
 print(var_3["A"][1])                                        # Dictionary time 1St columns name then row index number like these
 print(var_4)                                                 # Reflect the column id by name
+print(var_5)
