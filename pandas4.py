9ab2ca4c65bbb9b544da311b512bbf7fd1c8b94f
Predator <kungfuyoga99@gmail.com>
2026-04-08 00:47:24 +0530

    auto commit

--- pandas4.py
+++ pandas4.py
@@ -1,6 +1,8 @@
 import pandas as pd
 
 
+list = [1,2,3,4]
+list_2 =  [1,2,3]
 Dic = {
     "A":[1,2,3],
     "B":[2,4,6],
@@ -9,6 +11,9 @@ Dic = {
 Dic_2 = {
     "A":[10]
 }
-
+var = pd.DataFrame(list)
+print(var)
+var_2 = pd.DataFrame(list_2)
+print(var_2)
 var_f = pd.DataFrame(Dic)
 print(var_f)
