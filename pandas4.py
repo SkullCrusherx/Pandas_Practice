18c0297a3a7e4893e3cf35b4cc1cc40e50649aac
Predator <kungfuyoga99@gmail.com>
2026-04-08 00:53:06 +0530

    auto commit

---
+++ pandas4.py
@@ -0,0 +1,30 @@
+import pandas as pd
+
+
+list = [1,2,3,4]
+list_2 =  [1,2,3]
+Dic = {
+    "A":[1,2,3],
+    "B":[2,4,6],
+    "C":[3,6,9]
+}
+Dic_2 = {
+    "A":[10,11]
+}
+
+
+
+var = pd.DataFrame(list)
+var_2 = pd.DataFrame(list_2)
+var_3 = pd.DataFrame(Dic,index= ['A','B','C'])
+var_4 = pd.DataFrame(Dic_2)
+
+var_3['XX'] = var_3['A'] + var_3['B']
+var_3['XY'] = var_3['A'] - var_3['B']
+var_3['XZ'] = var_3['A'] / var_3['B']
+var_3['XA'] = var_3['A'] * var_3['B']
+
+var_3['F'] = var_3["A"] + var_4 ["A"]
+
+
+print(var_3)
