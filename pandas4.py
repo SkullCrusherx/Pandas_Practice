df4883ae04fd267f83526c71ea68c688ac2c426f
Predator <kungfuyoga99@gmail.com>
2026-04-08 00:39:49 +0530

    auto commit

--- pandas4.py
+++ pandas4.py
@@ -2,10 +2,24 @@ import pandas as pd
 
 list = [1,2,3,4]
 list2 = [5,6,7]
-
+Dic = {
+    "A":1,
+    "B":2,
+    "C":3
+}
+Dic_2 = {
+    "A":10
+}
+"""
 var = pd.DataFrame(list)
 var_2 = pd.DataFrame(list2)
 
 print(var + var_2)
 print(var - var_2)
-print(var * 2)
+print(var * 2)"""
+
+var = pd.DataFrame(Dic)
+var_2 = pd.DataFrame(Dic_2)
+print(var + var_2)
+print(var - var_2)
+
