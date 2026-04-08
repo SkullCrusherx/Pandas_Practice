716100c26035aff26a5f706af7eca9c76af5638b
Predator <kungfuyoga99@gmail.com>
2026-04-08 21:29:57 +0530

    auto commit

--- Pandas6.py
+++ Pandas6.py
@@ -11,7 +11,7 @@ df_4 = pd.read_csv("ML.csv",skiprows=4)                                 # Read f
 df_4 = pd.read_csv("ML.csv",skiprows=[4])                               # Read from element to skip rows number which is given
 df_5 = pd.read_csv("ML.csv",skiprows=[4,5])                             # Read from element to skip rows number which is given
 
-df_6 = pd.read_csv("ML.csv",index_col=[0])
+df_6 = pd.read_csv("ML.csv",index_col=[0,1])
 
 df_7 = pd.read_csv("ML.csv",header=None)                                # Given Another header 0 and 1
 df_8 = pd.read_csv("ML.csv",header=[1])                                 # number of index given in header it goes header
