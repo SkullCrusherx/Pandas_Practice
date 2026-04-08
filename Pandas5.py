5a12d1fcd549a9a02d7f4ae67d5c8cdbf8739eac
Predator <kungfuyoga99@gmail.com>
2026-04-08 21:09:22 +0530

    auto commit

--- Pandas5.py
+++ Pandas5.py
@@ -1,5 +1,8 @@
 import pandas as pd
 
-df = pd.read_csv("ML.csv",nrows=2)
+df = pd.read_csv("ML.csv")                                          # Read the CSV File
+df_1 = pd.read_csv("ML.csv", nrows = 2)               # Read Number of Rows how much Need From 0 to N
+df_2 = pd.read_csv("ML.csv", usecols = 2)             # Read Number of Rows how much Need From 0 to N
 
-print(df)
+
+print(df_2)
