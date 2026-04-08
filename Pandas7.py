f66f9faf961a4b35027d2e3ef8a74668d9604ddd
Predator <kungfuyoga99@gmail.com>
2026-04-08 21:17:15 +0530

    auto commit

--- Pandas7.py
+++ Pandas7.py
@@ -1,4 +1,21 @@
 import pandas as pd
+import numpy as np
 
 csv = pd.read_csv("ML.csv")
-print(csv)
+
+print(csv.index)                                                 #Showing the Detail of range index like step start end all
+print(csv.columns)                                               #showing all the column name mean header also
+print(csv.describe())                                            #Showing here count mean max and other values also
+print(csv.head(3))                                               #3 here how much data need to show here from Top
+print(csv.tail(3))                                               #3 here how much data need to show here from Bottom
+print(csv[:2])                                                   #from start to end also we check print using like these [start:end]
+print(type(csv[:2]))                                             #showing or reflect the data type
+print(csv.index.array)                                           #showing index all number into array(pandas)
+print(csv.columns.array)                                         #showing in array Column name or headers
+print(csv.to_numpy())                                            #all Data element convert into numpy array
+print(np.asarray(csv))                                           #import numpy 1st then np function as array call to make numpy array
+print(csv.sort_index(axis=0,ascending=False))                    #sort index means index wise sort and ascending use for not required for ascending theats why False otherWise True
+print(csv.loc[[0,1],["BHK","Price"]])                            #here ican get data using which data actually need using slicing [[Row number ],[column name ]]
+print(csv.loc[2],["BHK"])                                        #here ican get data using which data actually need using comma [["Colum name1","Colum name2"][5]Row]
+print(csv.iloc[2,1])                                             #iloc[] function particular data Reflect
+print(csv.drop("BHK",axis=1))                              #drop the particular colum here we can drop row wise column wise also
