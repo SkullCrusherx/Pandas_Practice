39eb2cac361c825fcd51372352214a130d1ebfd7
Predator <kungfuyoga99@gmail.com>
2026-04-08 21:25:49 +0530

    auto commit

--- Pandas6.py
+++ Pandas6.py
@@ -12,7 +12,7 @@ df_4 = pd.read_csv("ML.csv",skiprows=[4])                               # Read f
 df_5 = pd.read_csv("ML.csv",skiprows=[4,5])                             # Read from element to skip rows number which is given
 
 df_6 = pd.read_csv("ML.csv",header=None)                                # Given Another header 0 and 1
-df_7 = pd.read_csv("ML.csv",header=17)
+df_7 = pd.read_csv("ML.csv",header=18)
 
 #print(df["Price"])                                                                    # reflect the column which column required
 print(df_7)
