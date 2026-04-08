0e02f121e1e3c180c34a66c957474f22b7eda33b
Predator <kungfuyoga99@gmail.com>
2026-04-08 21:18:28 +0530

    auto commit

--- Pandas6.py
+++ Pandas6.py
@@ -6,5 +6,5 @@ df_2 = pd.read_csv("ML.csv", usecols = [0,1])                           # Read i
 df_3 = pd.read_csv("ML.csv", usecols = ["BHK","Price"])                 # Read column or name how much Need using comma [0,1]
 df_4 = pd.read_csv("ML.csv",skiprows=2)
 
-print(df["Price"])                                                                    # reflect the column which column required
-#print(df_3)
+#print(df["Price"])                                                                    # reflect the column which column required
+print(df_4)
