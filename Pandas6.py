a69af51fe68ba2c072d0adba39c826039b22d864
Predator <kungfuyoga99@gmail.com>
2026-04-08 21:24:36 +0530

    auto commit

--- Pandas6.py
+++ Pandas6.py
@@ -1,13 +1,17 @@
 import pandas as pd
 
 df = pd.read_csv("ML.csv")                                                            # Read the CSV File
+
 df_1 = pd.read_csv("ML.csv", nrows = 2)                                 # Read Number of Rows how much Need From 0 to N
+
 df_2 = pd.read_csv("ML.csv", usecols = [0,1])                           # Read index Number of column or name how much Need using comma [0,1]
 df_3 = pd.read_csv("ML.csv", usecols = ["BHK","Price"])                 # Read column or name how much Need using comma [0,1]
+
 df_4 = pd.read_csv("ML.csv",skiprows=4)                                 # Read from 0 to skip rows number = N time skip all
 df_4 = pd.read_csv("ML.csv",skiprows=[4])                               # Read from element to skip rows number which is given
 df_5 = pd.read_csv("ML.csv",skiprows=[4,5])                             # Read from element to skip rows number which is given
 
+df_6 = pd.read_csv("ML.csv",header=None)
 
 print(df["Price"])                                                                    # reflect the column which column required
-print(df_5)
+print(df_6)
