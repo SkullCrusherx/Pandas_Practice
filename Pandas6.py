b92e5dbc55dd730cf853c7eed334dc2c100dd8e8
Predator <kungfuyoga99@gmail.com>
2026-04-08 21:47:55 +0530

    auto commit

--- Pandas6.py
+++ Pandas6.py
@@ -8,14 +8,14 @@ df_2 = pd.read_csv("ML.csv", usecols = [0,1])                           # Read i
 df_3 = pd.read_csv("ML.csv", usecols = ["BHK","Price"])                 # Read column or name how much Need using comma [0,1]
 
 df_4 = pd.read_csv("ML.csv",skiprows=4)                                 # Read from 0 to skip rows number = N time skip all
-df_4 = pd.read_csv("ML.csv",skiprows=[4])                               # Read from element to skip rows number which is given
-df_5 = pd.read_csv("ML.csv",skiprows=[4,5])                             # Read from element to skip rows number which is given
+df_5 = pd.read_csv("ML.csv",skiprows=[4])                               # Read from element to skip rows number which is given
+df_6 = pd.read_csv("ML.csv",skiprows=[4,5])                             # Read from element to skip rows number which is given
 
-df_6 = pd.read_csv("ML.csv",index_col=[0])
+df_7 = pd.read_csv("ML.csv",index_col=[0])                              # make column as index using this if always it works column length - 1
 
-df_7 = pd.read_csv("ML.csv",header=None)                                # Given Another header 0 and 1
-df_8 = pd.read_csv("ML.csv",header=[1])                                 # number of index given in header it goes header
-df_9 = pd.read_csv("ML.csv",header=1)                                   # number of index given upon all Data will skip Both
+df_8 = pd.read_csv("ML.csv",header=None)                                # Given Another header 0 and 1
+df_9 = pd.read_csv("ML.csv",header=[1])                                 # number of index given in header it goes header
+df_10 = pd.read_csv("ML.csv",header=1)                                  # number of index given upon all Data will skip Both
 
 #print(df["Price"])                                                                    # reflect the column which column required
 print(df_6)
