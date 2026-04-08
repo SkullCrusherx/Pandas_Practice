693039a44c45a073d5639d5778e6b876092fe712
Predator <kungfuyoga99@gmail.com>
2026-04-08 23:42:16 +0530

    auto commit

--- Pandas5.py
+++ Pandas5.py
@@ -4,7 +4,7 @@ data = {
     "ID" : ['1A','1B','1C','1D'],                                   # Create Dictionary As 2D value set
     "Name" :['sayan','Rupa','Rahul','Anik']
 }
-df = pd.DataFrame(data,index=False)                                             # Make 2D Frame
+df = pd.DataFrame(data,index=None)                                             # Make 2D Frame
 
 print(df)
 
