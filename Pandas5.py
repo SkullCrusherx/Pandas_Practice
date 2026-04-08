07e288dadfa9d261fe4ea92d06a27d567219ea62
Predator <kungfuyoga99@gmail.com>
2026-04-08 21:13:15 +0530

    auto commit

--- Pandas5.py
+++ Pandas5.py
@@ -1,9 +1,10 @@
 import pandas as pd
 
-df = pd.read_csv("ML.csv")                                              # Read the CSV File
-df_1 = pd.read_csv("ML.csv", nrows = 2)                   # Read Number of Rows how much Need From 0 to N
-df_2 = pd.read_csv("ML.csv", usecols = [0,1])             # Read index Number of column or name how much Need using comma [0,1]
-df_3 = pd.read_csv("ML.csv", usecols = ["BHK","Price"])   # Read column or name how much Need using comma [0,1]
+df = pd.read_csv("ML.csv")                                                            # Read the CSV File
+df_1 = pd.read_csv("ML.csv", nrows = 2)                                 # Read Number of Rows how much Need From 0 to N
+df_2 = pd.read_csv("ML.csv", usecols = [0,1])                           # Read index Number of column or name how much Need using comma [0,1]
+df_3 = pd.read_csv("ML.csv", usecols = ["BHK","Price"])                 # Read column or name how much Need using comma [0,1]
 
 
-print(df_3)
+print(df["BHK"])
+#print(df_3)
