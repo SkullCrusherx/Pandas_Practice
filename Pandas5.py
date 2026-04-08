afe74863a3e3276fa4e4a2e0102c24d6bbbb62f5
Predator <kungfuyoga99@gmail.com>
2026-04-08 23:41:12 +0530

    auto commit

--- Pandas5.py
+++ Pandas5.py
@@ -1,9 +1,9 @@
 import pandas as pd
 
 data = {
-    "ID" : ['1A','1B','1C','1D'],
+    "ID" : ['1A','1B','1C','1D'],                                   # Create Dictionary As 2D value set
     "Name" :['sayan','Rupa','Rahul','Anik']
 }
-df = pd.DataFrame(data)
+df = pd.DataFrame(data)                                             # Make 2D Frame
 print(df)
 #df.to_csv('data.csv')
