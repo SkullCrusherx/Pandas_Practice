9d36c567fab32b8a9c7a1636f12b8c75e7f85269
Predator <kungfuyoga99@gmail.com>
2026-04-10 00:14:17 +0530

    8/4/26

--- Pandas7.py
+++ Pandas7.py
@@ -9,12 +9,12 @@ print(csv.describe())                                            #Showing here c
 print(csv.head(3))                                               #3 here how much data need to show here from Top
 print(csv.tail(3))                                               #3 here how much data need to show here from Bottom
 print(csv[:2])                                                   #from start to end also we check print using like these [start:end]
-print(type(csv[:2]))                                             #showing or reflect the data type
+print(type(csv))                                                 #showing or reflect the data type
 print(csv.index.array)                                           #showing index all number into array(pandas)
 print(csv.columns.array)                                         #showing in array Column name or headers
 print(csv.to_numpy())                                            #all Data element convert into numpy array
 print(np.asarray(csv))                                           #import numpy 1st then np function as array call to make numpy array
-print(csv.sort_index(axis=0,ascending=False))                    #sort index means index wise sort and ascending use for not required for ascending theats why False otherWise True
+print(csv.sort_index(axis=0,ascending=False))                    #sort index means index wise sort and ascending use for not required for ascending that's why False otherWise True
 print(csv.loc[[0,1],["BHK","Price"]])                            #here ican get data using which data actually need using slicing [[Row number ],[column name ]]
 print(csv.loc[2],["BHK"])                                        #here ican get data using which data actually need using comma [["Colum name1","Colum name2"][5]Row]
 print(csv.iloc[2,1])                                             #iloc[] function particular data Reflect
