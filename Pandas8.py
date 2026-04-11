4fe943169fce635e0252435987ce49c60f247346
Predator <kungfuyoga99@gmail.com>
2026-04-11 23:11:01 +0530

    auto commit

--- Pandas8.py
+++ Pandas8.py
@@ -2,11 +2,13 @@ import pandas as pd
 
 csv = pd.read_csv("new.csv")
 
-print(csv.dropna())                             # All Nan drop all new data set show
-print(csv.dropna(axis=0))                       # 
-print(csv.dropna(axis=1))                       #
-print(csv.dropna(how="all"))                    #
-print(csv.dropna(how="any"))                    #
-print(csv.dropna(subset=["BHK"]))               #
-print(csv.dropna(inplace=True))                 #
-print(csv.dropna(thresh=2))                     #
+print(csv.dropna())                             # All 'Nan' drop all new data set show
+print(csv.dropna(axis=0))                       # All 'Nan' drop row wise bcz axis = 0
+print(csv.dropna(axis=1))                       # All 'Nan' drop column wise bcz axis = 1
+print(csv.dropna(how="all"))                    # If All value of dataset Nan it will Drop
+print(csv.dropna(how="any"))                    # if any data of a row is Nan it will Drop
+print(csv.dropna(subset=["BHK"]))               # All 'Nan' drop column wise bcz subset = ['column name']
+print(csv.dropna(inplace=True))                 # All 'Nan' drop all new data set show
+print(csv.dropna(thresh=2))                     # minimum 2 value hold in a row other wise it will Drop
+
+
