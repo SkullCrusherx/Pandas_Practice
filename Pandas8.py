aa5316f2231b08cede2f6237575055f9cf53371c
Predator <kungfuyoga99@gmail.com>
2026-04-11 23:04:12 +0530

    auto commit

--- Pandas8.py
+++ Pandas8.py
@@ -2,4 +2,6 @@ import pandas as pd
 
 csv = pd.read_csv("new.csv")
 
-print(csv.dropna())                             # 
+print(csv.dropna())                             #
+print(csv.dropna(axis=0))                       #
+print(csv.dropna(axis=1))                       #
