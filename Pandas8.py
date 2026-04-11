fbc112c692fbf79b4029f353cc9d428491d25bee
Predator <kungfuyoga99@gmail.com>
2026-04-11 23:33:44 +0530

    auto commit

--- Pandas8.py
+++ Pandas8.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 csv = pd.read_csv("new.csv")
-
+'''
 print(csv.dropna())                             # All 'Nan' drop all new data set show
 print(csv.dropna(axis=0))                       # All 'Nan' drop row wise bcz axis = 0
 print(csv.dropna(axis=1))                       # All 'Nan' drop column wise bcz axis = 1
@@ -11,6 +11,9 @@ print(csv.dropna(subset=["BHK"]))               # All 'Nan' drop column wise bcz
 print(csv.dropna(inplace=True))                 # All 'Nan' drop all new data set show
 print(csv.dropna(thresh=2))                     # minimum 2 value hold in a row otherwise it will Drop
 
-print(csv.fillna("Data"))                       # Blank 
+print(csv.fillna("Data"))                       # Blank Data will update or Fill with 'Data'
+print(csv.fillna({'Column name':'Data'}))       # column wise empty data will fill
+'''
+csv.fillna(method = 'ffill')
 
 
