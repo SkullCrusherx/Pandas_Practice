b7048af7312f6d29179c20fa75775d38987b498b
Predator <kungfuyoga99@gmail.com>
2026-04-11 23:41:15 +0530

    auto commit

--- Pandas8.py
+++ Pandas8.py
@@ -7,7 +7,7 @@ print(csv.dropna(axis=0))                           # All 'Nan' drop row wise bc
 print(csv.dropna(axis=1))                           # All 'Nan' drop column wise bcz axis = 1
 print(csv.dropna(how="all"))                        # If All value of dataset Nan it will Drop
 print(csv.dropna(how="any"))                        # if any data of a row is Nan it will Drop
-print(csv.dropna(subset=["BHK"]))                   # All 'Nan' drop column wise bcz subset = ['column name']
+print(csv.dropna(subset=["Day"]))                   # All 'Nan' drop column wise bcz subset = ['column name']
 print(csv.dropna(inplace=True))                     # All 'Nan' drop all new data set show
 print(csv.dropna(thresh=2))                         # minimum 2 value hold in a row otherwise it will Drop
 
