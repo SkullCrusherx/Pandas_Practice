b08685d7573b10263d95cea141b775dfaee29144
Predator <kungfuyoga99@gmail.com>
2026-04-11 23:44:55 +0530

    auto commit

--- Pandas8.py
+++ Pandas8.py
@@ -13,11 +13,11 @@ print(csv.dropna(thresh=2))                         # minimum 2 value hold in a 
 
 print(csv.fillna("Data"))                           # Blank Data will update or Fill with 'Data'
 print(csv.fillna({'Column name':'Data'}))           # column wise empty data will fill
-#print(csv.fillna(method = 'ffill/bfill'))           # Fill data before data with or forward with data
-#print(csv.fillna(method = 'ffill/bfill',axis=0/1))  # Fill data before data with or forward with data using axis 0 means row and column means 1
+csv.ffill(axis=0/1)                                         # Fill data forward with data axis 0 means row and column means 1
+csv.bfill(axis=0/1)                                         # Fill data before data with using axis 0 means row and column means 1
 print(csv.fillna("Data",inplace=True))        # All 'Nan' Fill all new data set show
 print(csv.fillna("Data",limit=2))             # All 'Nan' Fill all column wise  and from top to bottom limit wise data if 2 then blank top to bottom 2 data will replace only
-csv.ffill()
-csv.bfill()
+
+
 
 
