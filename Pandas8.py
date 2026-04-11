9df9cd9014062ef3293a71e750a7f0ec25ead351
Predator <kungfuyoga99@gmail.com>
2026-04-11 23:06:56 +0530

    auto commit

--- Pandas8.py
+++ Pandas8.py
@@ -2,6 +2,11 @@ import pandas as pd
 
 csv = pd.read_csv("new.csv")
 
-print(csv.dropna())                             #
+print(csv.dropna())                             # all 
 print(csv.dropna(axis=0))                       #
 print(csv.dropna(axis=1))                       #
+print(csv.dropna(how="all"))                    #
+print(csv.dropna(how="any"))                    #
+print(csv.dropna(subset=["BHK"]))               #
+print(csv.dropna(inplace=True))                 #
+print(csv.dropna(thresh=2))                     #
