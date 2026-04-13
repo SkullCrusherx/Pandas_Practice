9f6a541857051881f6db538f6f1ef24a8068e248
Predator <kungfuyoga99@gmail.com>
2026-04-14 01:42:13 +0530

    auto commit

--- Pandas10.py
+++ Pandas10.py
@@ -10,10 +10,21 @@ h = pd.merge(pd.merge(df1,df2,how="right"),df3,how='right')
 print(g)
 print()
 print(h)
-print()"""
+print()
 
 
 csv = pd.read_csv('new.csv')
 csv['Time'] = csv['Time'].astype('int64')
 csv['Time'] = csv["Time"].interpolate()
+print(csv)"""
+
+import pandas as pd
+
+csv = pd.read_csv('new.csv')
+
+csv['Time'] = pd.to_datetime(csv['Time'], errors='coerce')
+csv['Time'] = csv['Time'].view('int64')
+csv['Time'] = csv['Time'].interpolate()
+csv['Time'] = pd.to_datetime(csv['Time'])
+
 print(csv)
