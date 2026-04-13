bcff311c295f2ddf3495ab867601686a253f3e76
Predator <kungfuyoga99@gmail.com>
2026-04-14 01:34:14 +0530

    auto commit

--- Pandas10.py
+++ Pandas10.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-df1 = pd.DataFrame({"A":[1,2,3]})
+"""df1 = pd.DataFrame({"A":[1,2,3]})
 df2 = pd.DataFrame({"A":[1,2,4]})
 df3 = pd.DataFrame({"A":[1,2,5]})
 
@@ -10,4 +10,7 @@ h = pd.merge(pd.merge(df1,df2,how="right"),df3,how='right')
 print(g)
 print()
 print(h)
-print()
+print()"""
+
+
+csv = pd.read_csv()
