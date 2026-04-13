33170998eb1d0234ba4d059ca256c01288aad463
Predator <kungfuyoga99@gmail.com>
2026-04-14 01:22:21 +0530

    8/4/26

---
+++ Pandas10.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+df1 = pd.DataFrame({"A":[1,2,3]})
+df2 = pd.DataFrame({"A":[1,2,4]})
+df3 = pd.DataFrame({"A":[1,2,5]})
+
+g = pd.merge(pd.merge(df1,df2,how="left"),df3,how='left')
+h = pd.merge(pd.merge(df1,df2,how="right"),df3,how='right')
+
+print(g)
+print()
+print(h)
+print()
