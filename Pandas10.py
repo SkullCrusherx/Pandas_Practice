f11a74bf587f7c3cc9364b34cc07603bab7f88a4
Predator <kungfuyoga99@gmail.com>
2026-04-16 22:48:54 +0530

    auto commit

--- Pandas10.py
+++ Pandas10.py
@@ -2,9 +2,10 @@ import pandas as pd
 
 df1 = pd.DataFrame({"A":[1,2,3],"B":[4,5,6]})
 df2 = pd.DataFrame({"A":[1,2,4],"B":[4,5,6]})
+df3 = pd.DataFrame({"A":[1,2,4],"C":[4,5,6]})
 
 
-g = pd.merge(df1,df2,on = "A" )
+g = pd.merge(df1,df3,on = "A" )
 h = pd.merge(df1,df2,how = "left")
 i = pd.merge(df1,df2,how = "right")
 
