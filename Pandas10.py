ea68013cc70899817f6cbbe1854991c1cdbdc821
Predator <kungfuyoga99@gmail.com>
2026-04-16 22:40:20 +0530

    auto commit

--- Pandas10.py
+++ Pandas10.py
@@ -4,13 +4,13 @@ df1 = pd.DataFrame({"A":[1,2,3],"B":[4,5,6]})
 df2 = pd.DataFrame({"A":[1,2,4],"B":[4,5,6]})
 
 
-g = pd.merge(df1,df2,on = "A")
+g = pd.merge(df1,df2,on = "A" )
 h = pd.merge(df1,df2,how = "left")
 i = pd.merge(df1,df2,how = "right")
 
 
-print(g)
+print("GG",g)
 print()
-print(h)
+print("HH",h)
 print()
-print(i)
+print("II",i)
