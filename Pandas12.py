d2f8165f7d67169dca671ef045774b3ed953c6c5
Predator <kungfuyoga99@gmail.com>
2026-04-18 23:05:18 +0530

    auto commit

--- Pandas12.py
+++ Pandas12.py
@@ -1,5 +1,2 @@
 import pandas as pd
 
-pd.set_option('display.max_rows', None)      # show all rows
-pd.set_option('display.width', None)         # avoid line wrapping
-pd.set_option('display.max_colwidth', None)  # show full text in columns
