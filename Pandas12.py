10f97eacf737525c4fbf639d2f879d0a60092adb
Predator <kungfuyoga99@gmail.com>
2026-04-18 23:45:52 +0530

    auto commit

--- Pandas12.py
+++ Pandas12.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
 var = pd.DataFrame({'A':[1,2,3,4,5],"B":[6,7,8,9,10]},index=[2,3,4,7,9],columns=['X','Y','Z','H','J'])
-var_2 = pd.DataFrame({'C':[1,2,3,4],"D":[6,7,8,9]},index=[2,3,4,7],columns=['X','Y','Z','H'])
+var_2 = pd.DataFrame({'C':[1,2,3,4],"D":[6,7,8,9]},index=[2,3,4,7],columns=['K','L','M','N'])
 
 print(var.join(var_2))
