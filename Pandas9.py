2e95570c8afe69c6b12f38a1856eda6a06173946
Predator <kungfuyoga99@gmail.com>
2026-04-19 20:00:23 +0530

    8/4/26

--- Pandas9.py
+++ Pandas9.py
@@ -9,7 +9,6 @@ csv.replace([1,2,3],[44,55,66],inplace=True)                                    
 csv.replace([1,2,3],44,inplace=True)                                             # Single Value also change replace all The Data
 csv.replace({"Day":[1,2,3,4,5,6,7,8,9]},44,inplace=True)                         # Dictionary Given and replace to NAn Value 44
 csv = csv.astype(str)                                                                           # if i m working with int then 1st I need to convert into string using this parameter
-
 #csv.replace(r'^\d+$','FF',regex=True,inplace=True)                                             # After I can replace old value to new value r for raw data ^ for starting \d all number + increase $ for end
 
 
@@ -17,5 +16,11 @@ csv = csv.astype(str)                                                           
 #csv[['Age','No']]=csv[['Age','No']].interpolate()                                              # objective Data Contain all str,Float,int ,but I need specific column to change like this
 #csv.interpolate(limit=n)                                                                       # n = anyvalue int how many data need to chnage
 #csv.interpolate(limit_area='inside',inplace=True)                                              # Data will work all data liner wise change
+#csv.interpolate(limit_direction='forward/backward/both',inplace=True)                          # fil data by forward linear wise back word linear wise and both
+
+#csv.interpolate().ffill()
+#csv.interpolate().bfill()
+
+
 
 print(csv)
